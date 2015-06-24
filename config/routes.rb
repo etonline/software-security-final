2cc21e2259474931a2c044b88cd7577da6b67424
Jacky Lin <online.et@gmail.com>
2015-06-25 01:58:18 +0800

    Change query from post to get method.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :users
   resources :articles
-  post 'users/query' => 'users#query'
+  get 'user/query' => 'users#query'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
